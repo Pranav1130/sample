e05ff477291b3973f365a46fd804975aa85d9b64
panav <pranav113025@gmail.com>
2022-11-18 03:53:00 +0530

    commit

---
+++ bin/sample.dart
@@ -0,0 +1,7 @@
+/// entry point >> main function
+/// syntax >> returntype  functionname() {}
+void main(){
+  String name= "hello world";
+  print(name);
+  print(1253681);
+}
---
+++ bin/sample_project.dart
@@ -0,0 +1,5 @@
+import 'package:sample_project/sample_project.dart' as sample_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sample_project.calculate()}!');
+}
