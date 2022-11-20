3d0d416e772c658c87b17a52b13d3fb8d4af899b
panav <pranav113025@gmail.com>
2022-11-21 03:13:34 +0530

    commit

---
+++ bin/variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  String name = "Pranav";
+  int age = 20;
+  int phone = 9497143687;
+  String email = "Pranav113025@gmail.com";
+  double mark = 8.55;
+
+  print("^^^^^^^^^^MY RESUME^^^^^^^^^^");
+  print("my name is $name");  ///string interpolation
+  print("i am $age years old");
+  print("my phone num is $phone");
+  print("i have $mark cgpa in my gratuation");
+  print("my email id is $email");
+}
